e30418372522bbf502eb82ab332db348e189c123
[--global] <[--global]>
2023-02-12 15:38:42 -0500

    need to finish remove functionality

--- src/pages/CalendarComponent.js
+++ src/pages/CalendarComponent.js
@@ -1,8 +1,10 @@
-import React, { useCallback, useState, useMemo, Fragment, useEffect } from 'react'
+import React, { useCallback, useState, useMemo, Fragment, useEffect, useRef } from 'react';
+import ReactDOM from "react-dom";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
 import { useNavigate  } from "react-router-dom";
+import { Overlay, Tooltip, Button, Row, Col } from "react-bootstrap";
 
 import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
 import "react-big-calendar/lib/css/react-big-calendar.css";
@@ -27,10 +29,11 @@ export default function CalendarComponent({
         id: 1,
         start: moment().toDate(),
         end: moment().add(1, "hours").toDate(),
-        title: "Some title",
+        title: currentUser,
         user: currentUser
       },
     ];
+    console.log("localStorage events: ", JSON.parse(localStorage.getItem("events")));
     const [myEvents, setEvents] = useState(events);
     const [color, setColor ] = useState("#265985");
     const navigate = useNavigate();
@@ -38,9 +41,11 @@ export default function CalendarComponent({
 
     React.useEffect(()=>{
       console.log("myEvents: ", myEvents);
+      localStorage.setItem("events", JSON.stringify(myEvents));
     },[myEvents])
+
+
     React.useEffect(()=>{
-      console.log("currentUser: ", currentUser);
       if(!currentUser || !currentUser.length){
         navigate("/Boss_Scheduler/userselect");
       }
@@ -48,6 +53,7 @@ export default function CalendarComponent({
 
     function handleColorChange(color){
       setColor(color.hex);
+      localStorage.setItem("color", color.hex);
     }
     const handleEventDrop = useCallback(
       ({ event, start, end, isAllDay: droppedOnAllDaySlot = false }) => {
@@ -78,11 +84,20 @@ export default function CalendarComponent({
     )
     const handleSelectSlot = useCallback(
       (event) => {
-        setEvents((prev) => {
-          const idList = prev.map((item) => item.id)
-          const newId = Math.max(...idList) + 1
-          return [...prev, { ...event, id: newId, user: currentUser }]
-        })
+        //if user selects somewhere in the calendar (not top area) or clicks and drags
+        if(event.box ||event.bounds){
+          setEvents((prev) => {
+            const idList = prev.map((item) => item.id)
+            const newId = Math.max(...idList) + 1
+            return [...prev, { 
+              ...event, 
+              id: newId, 
+              user: currentUser,
+              title: currentUser
+            }]
+          })
+        }
+
       },
       [setEvents]
     )
@@ -97,6 +112,111 @@ export default function CalendarComponent({
       }),
       [color]
     )
+    const removeEvent = useCallback(
+      (event) => {
+        const remove_confirm = window.confirm("Would you like to remove this event?");
+        if(remove_confirm === true){
+          console.log("you removed: ", event);
+          setEvents((prev) => {
+            const events = [...prev];
+            const index = events.indexOf(event.event);
+            events.splice(index,1);
+            return {events};
+          })
+
+          console.log("after removing: ", myEvents);
+        }
+      },
+      []
+    )
+
+    // function removeEvent(event){
+    //   const remove_confirm = window.confirm("Would you like to remove this event?")
+    //   if(remove_confirm === true){
+    //     console.log("you removed: ", event);
+    //     this.setState((prevState, props) => {
+    //       const events = [...prevState.events]
+    //       const idx = events.indexOf(pEvent)
+    //       events.splice(idx, 1);
+    //       return { events };
+    //     });
+    //   }
+    // }
+
+    // onSelectEvent(pEvent) {
+    //   const r = window.confirm("Would you like to remove this event?")
+    //   if(r === true){
+        
+    //     this.setState((prevState, props) => {
+    //       const events = [...prevState.events]
+    //       const idx = events.indexOf(pEvent)
+    //       events.splice(idx, 1);
+    //       return { events };
+    //     });
+    //   }
+    // }
+
+    const TooltipContent = ({onClose, event}) => {
+      console.log("event: ", event);
+      console.log("start date: ", moment(event.event.start).format("h:mma"));
+      const start_time = moment(event.event.start).format("h:mma");
+      const end_time = moment(event.event.end).format("h:mma")
+      return (
+        <div className="tooltip-container bg-slate-50 p-2 rounded-md">
+          <div className="flex justify-between">
+            <div>{event.title}</div>
+            <div
+              className="cursor-pointer font-light text-xs p-px rounded-md text-red-500 border border-gray-300 w-max"
+              onClick={()=>removeEvent(event)}
+            >DELETE</div>
+          </div>
+          <div>{start_time} - {end_time}</div>
+            
+        </div>
+      );
+    }
+    function EventComponent(event) {
+      const [showTooltip, setShowTooltip] = useState(false);
+    
+      const closeTooltip = () => {
+        setShowTooltip(false);
+      };
+    
+      const openTooltip = () => {
+        setShowTooltip(true);
+      }
+      const ref = useRef(null);
+    
+      const getTarget = () => {
+        return ReactDOM.findDOMNode(ref.current);
+      };
+    
+      return (
+        <div 
+          className="event-container h-full" 
+          // onMouseLeave={closeTooltip}
+          ref={ref}
+          onMouseOver={openTooltip}
+        >
+          <span  className="font-light text-sm">{event.title}</span>
+          <Overlay
+            rootClose
+            target={getTarget}
+            show={showTooltip}
+            placement="top"
+            onHide={closeTooltip}
+            className="overlay-container"
+          >
+            <Tooltip 
+              id="event-tooltip"
+              // onMouseLeave={closeTooltip}
+            >
+              <TooltipContent event={event} onClose={closeTooltip}/>
+            </Tooltip>
+          </Overlay>
+        </div>
+      );
+    }
     return(
         <div className="wrapper">
             <div className="flex justify-between items-center p-2">
@@ -110,8 +230,11 @@ export default function CalendarComponent({
                 />
             </div>
             <DnDCalendar
+              tooltipAccessor={null}
+              components={{ event: EventComponent }}
               defaultDate={moment().toDate()}
               defaultView="week"
+              scrollToTime={moment().toDate()}
               events={myEvents}
               localizer={localizer}
               onEventDrop={handleEventDrop}
@@ -129,3 +252,7 @@ export default function CalendarComponent({
 
     )
 }
+
+
+//TODO
+//have to change it so instead of sunday - monday with the dates, it's wednesday to monday without any dates
